c3666e058a10c96188909fcbdf950f40ee5158dc
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-12-04 09:37:35 +0530

    images diaplyed from db

--- lib/views/places.dart
+++ lib/views/places.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Places extends StatefulWidget {
   const Places({super.key});
@@ -12,7 +9,7 @@ class Places extends StatefulWidget {
 }
 
 class _PlacesState extends State<Places> {
-
+  final placesCollection = FirebaseFirestore.instance.collection("places");
   late final List<String> naturesImages;
   late ScrollController scrollController;
   ValueNotifier<int> highLightedIndex = ValueNotifier<int>(0);
@@ -27,50 +24,79 @@ class _PlacesState extends State<Places> {
       "assets/images/nature1.jpeg",
       "assets/images/nature2.jpeg",
       "assets/images/nature3.jpeg",
-      "assets/images/nature4.png"
+      "assets/images/nature4.png",
     ];
     super.initState();
+  }
+
+  Stream<List<dynamic>> fetchPlaces() {
+    return placesCollection.snapshots().map(
+      (event) => event.docs.map((ele) => ele.data()).toList(),
+    );
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    centerScreen = MediaQuery.of(context).size.width/2;
+    centerScreen = MediaQuery.of(context).size.width / 2;
   }
 
   // ui issue
   // computation
 
-  void findCenterImage(){
-    final centerPosition = scrollController.offset+centerScreen;
-    double index = centerPosition/cardWidth;
+  void findCenterImage() {
+    final centerPosition = scrollController.offset + centerScreen;
+    double index = centerPosition / cardWidth;
     highLightedIndex.value = index.floor();
   }
 
   @override
   Widget build(BuildContext context) {
+    fetchPlaces();
     return SizedBox(
       height: 350,
-      child: ValueListenableBuilder(
-          valueListenable: highLightedIndex,
-          builder: (context, notifier, _) {
-          return ListView.builder(
-              controller: scrollController,
-              scrollDirection: Axis.horizontal,
-              itemCount: naturesImages.length,
-              itemBuilder: (context, index){
-                return AnimatedContainer(
-                  margin: EdgeInsets.symmetric(vertical: 25),
-                  duration: Duration(milliseconds: 400),
-                  transform: notifier==index?Matrix4.translationValues(0, -20, 0):Matrix4.translationValues(0, 0, 0),
-                  padding: const EdgeInsets.all(8.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: Image.asset(naturesImages[index], width: 200, height: 390, fit: BoxFit.fill),
-                  ),
+      child: StreamBuilder(
+        stream: fetchPlaces(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<dynamic> places = snapshot.data!;
+            return ValueListenableBuilder(
+              valueListenable: highLightedIndex,
+              builder: (context, notifier, _) {
+                return ListView.builder(
+                  controller: scrollController,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: places.length,
+                  itemBuilder: (context, index) {
+                    return AnimatedContainer(
+                      margin: EdgeInsets.symmetric(vertical: 25),
+                      duration: Duration(milliseconds: 400),
+                      transform:
+                          notifier == index
+                              ? Matrix4.translationValues(0, -20, 0)
+                              : Matrix4.translationValues(0, 0, 0),
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.network(
+                          errorBuilder: (context, error, stackTrace){
+                            return Image.asset(naturesImages[0]);
+                          },
+                          places[index]['images'][0],
+                          width: 200,
+                          height: 390,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
                 );
-          });
-        }
+              },
+            );
+          } else {
+            return SizedBox();
+          }
+        },
       ),
     );
   }
